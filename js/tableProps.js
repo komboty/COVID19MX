430fad7f2ceaa81f2ecd93f80e2d665a31079f99
parker <komubk@gmail.com>
2020-03-25 04:39:18 -0600

    Rangos de edad funciona en vista

--- js/tableProps.js
+++ js/tableProps.js
@@ -1,4 +1,17 @@
-let estatoTxt = document.getElementById('estadoT');
+let estadoTxt = document.getElementById('estadoT');
+
+let listTab = document.getElementById('list-tab');
+
+let rangosEdad = Object.keys(statesData.features[0].properties.edad);
+for (let index = 0; index < rangosEdad.length; index++) {
+    listTab.innerHTML += getGroupItemHTML('edad', rangosEdad[index]);
+}
+
+/*let KeyProcedencia = Object.keys(statesData.features[0].properties.procedencia);
+for (let index = 0; index < KeyProcedencia.length; index++) {
+    listTab.innerHTML += getGroupItemHTML('procedencia', KeyProcedencia[index]);
+}*/
+
 let numTotal = document.getElementById('totalT');
 let barTotal = document.getElementById('barTotalT');
 let numMujeres = document.getElementById('mujeresT');
@@ -6,35 +19,64 @@ let barMujeres = document.getElementById('barMujeresT');
 let numHombres = document.getElementById('hombresT');
 let barHombres = document.getElementById('barHombresT');
 
-
 function updateTable(props) {
-    estatoTxt.innerHTML = '<h5>' + props.estado + '</h5>';
-    
-    numTotal.innerHTML = '<h5>' + props.confirmados + '</h5>';
-    barTotal.style.width = props.confirmados + '%';
-    barTotal.setAttribute("aria-valuenow", props.confirmados)
-    
-    numMujeres.innerHTML = '<h6>' + props.mujer + '</h6>';
-    barMujeres.style.width = props.mujer + '%';
-    barMujeres.setAttribute("aria-valuenow", props.mujer)
-    
-    numHombres.innerHTML = '<h6>' + props.hombre + '</h6>';
-    barHombres.style.width = props.hombre + '%';
-    barHombres.setAttribute("aria-valuenow", props.hombre)
+    console.log(props);
+    setHElement(estadoTxt, props.estado, 5);
+    setValorGroupItem(numTotal, 5, barTotal, props.confirmados);
+    setValorGroupItem(numMujeres, 6, barMujeres, props.mujer);
+    setValorGroupItem(numHombres, 6, barHombres, props.hombre);
+    setValueEdad('edad', 'baredad', rangosEdad, props.edad);
 }
 
 function resertTable() {
-    estatoTxt.innerHTML = '<h5>Estado</h5>';
+    setHElement(estadoTxt, 'Estado', 5);
+    setValorGroupItem(numTotal, 5, barTotal, 0);
+    setValorGroupItem(numMujeres, 6, barMujeres, 0);
+    setValorGroupItem(numHombres, 6, barHombres, 0);
+    setValueEdad('edad', 'baredad', rangosEdad);
+}
 
-    numTotal.innerHTML = '<h5>0</h5>';
-    barTotal.style.width = '0%';
-    barTotal.setAttribute("aria-valuenow", 0)
+function setValorGroupItem(elementNumero, h, elementBar, valor) {
+    setHElement(elementNumero, valor, h);
+    elementBar.style.width = valor + '%';
+    elementBar.setAttribute("aria-valuenow", valor)
+}
 
-    numMujeres.innerHTML = '<h6>0</h6>';
-    barMujeres.style.width = '0%';
-    barMujeres.setAttribute("aria-valuenow", 0)
+function setHElement(element, valor, h) {
+    element.innerHTML = '<h' + h + '>' + valor + '</h' + h + '>';
+}
 
-    numHombres.innerHTML = '<h6>0</h6>';
-    barHombres.style.width = '0%';
-    barHombres.setAttribute("aria-valuenow", 0)
+function setValueEdad(keyNumElement, keyBarElement, ObjKeys, obj) {
+    let numRango, barRango, key, valor;
+
+    for (let index = 0; index < ObjKeys.length; index++) {
+        key = ObjKeys[index];
+        valor = obj ? obj[key] : 0;
+        numRango = document.getElementById(keyNumElement + key + 'T');
+        barRango = document.getElementById(keyBarElement + key + 'T');
+
+        setValorGroupItem(numRango, 6, barRango, valor);
+    }
+}
+
+function click(params) {
+    $('#myList a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+}
+
+function getGroupItemHTML(key, id) {
+    return '<a class="list-group-item list-group-item-light list-group-item-action" id="list-' + key + id + '-list"'
+        + 'data-toggle="list" role="tab" aria-controls="' + key + id + '">'
+        + '<div class="row no-gutters">'
+        + '<div class="col-md-8"><h6>' + key + ' ' + id + '</h6></div>'
+        + '<div id="' + key + id + 'T" class="col-md-4 text-right"><h6>0</h6>'
+        + '</div>'
+        + '</div>'
+        + '<div class="progress">'
+        + '<div id="bar' + key + id + 'T" class="progress-bar bg-warning" role="progressbar" style="width: 0%;" '
+        + 'aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>'
+        + '</div>'
+        + '</a>';
 }
